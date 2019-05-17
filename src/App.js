a596699db88707a1c2a01ac2a125dda5240c19f4
Stanislav <stanislav.ever@gmail.com>
2019-05-18 01:40:48 +0300

    added new test_export component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
+import MyTestComponent from './TestExport';
 
 
 
 const App = () => {
   return (
       <div className="App">
-        Hello world
+        Hello world;<br/>
+        <Header />
+        <MyTestComponent />
       </div>
+  );
+}
+
+
+const Header = () => {
+  return(
+    <div className="my_test_class">
+      Hello. i'm from another component;
+    </div>
   );
 }
 
---
+++ src/TestExport.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyTestComponent = () => {
+    return (
+        <div className="testcomponenta">
+            My name is TestComponent!!!
+        </div>
+    );
+}
+
+export default MyTestComponent;
